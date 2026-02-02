6a869027d21daf093c0568c654f221eb743687b4
YoYo-Head <yoyohead438@gmail.com>
2026-02-02 21:44:08 +1100

    (v26.1.0) Tank Drive
    * Created code for tank drive in the Drive System
    
    * Created a Config folder which will house java files containing variables for systems
    
    * Created Init code for the Drive System

---
+++ src/main/java/frc/robot/Configs/DriveSystemConfig.java
@@ -0,0 +1,7 @@
+package frc.robot.Configs;
+
+public class DriveSystemConfig {
+    public double SpeedDivisor = 1;
+    public double TurnDivisor = 0.75;
+
+}
--- src/main/java/frc/robot/Systems/DriveSystem.java
+++ src/main/java/frc/robot/Systems/DriveSystem.java
@@ -1,13 +1,46 @@
 package frc.robot.Systems;
 
-import org.ejml.equation.Variable;
-
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import java.frc.robot.Configs.DriveSystemConfig;
 
 public class DriveSystem {
-
-    public drive(SparkMax RightMotorA, SparkMax RightMotorB, SparkMax LeftMotorA, SparkMax LeftMotorB) {
+    /* CREATING VARIABLES FOR DIFFERENT THINGS */
+    // Creating 'drive'
+    private DifferentialDrive drive;
+    
+    // Creating all the different SparkMaxes for driving motors
+    private final SparkMax rightMotorA = new SparkMax(1, SparkMax.MotorType.kBrushed);
+    private final SparkMax rightMotorB = new SparkMax(2, SparkMax.MotorType.kBrushed);
+    private final SparkMax leftMotorA = new SparkMax(3, SparkMax.MotorType.kBrushed);
+    private final SparkMax leftMotorB = new SparkMax(4, SparkMax.MotorType.kBrushed);
+    
+    @SuppressWarnings("removal")
+    public void driveSystemInit() {
+        SparkMaxConfig rightLeaderConfig = new SparkMaxConfig();
+        rightLeaderConfig.inverted(true);
+        rightMotorA.configure(rightLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    
+        /* CONFIGURING THE FOLLOWER MOTORS */
+        // On right side, motor B follows motor A
+        SparkMaxConfig followerConfigRight = new SparkMaxConfig();
+        followerConfigRight.follow(rightMotorA.getDeviceId(), false);
+        rightMotorB.configure(followerConfigRight, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
         
+        // On left side, motor B follows motor A
+        SparkMaxConfig followerConfigLeft = new SparkMaxConfig();
+        followerConfigLeft.follow(leftMotorA.getDeviceId(), false); 
+        leftMotorB.configure(followerConfigLeft, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+    
+        /* SETTING UP THE DRIVE TRAIN */
+        // Setting up what motors are going to be used in 'drive' control system
+        drive = new DifferentialDrive(leftMotorA::set, rightMotorA::set); 
+    
 
 
     }
