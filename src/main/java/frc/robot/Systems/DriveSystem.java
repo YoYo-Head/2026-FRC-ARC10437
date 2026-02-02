b285ce49d67f302170c6082165ded136d68e70d3
YoYo-Head <yoyohead438@gmail.com>
2026-02-02 20:39:56 +1100

    (v26.0.0) First Commit
    * Created FRC project so it contains all necessary files for building
    
    * Added a systems folders that contains the following java files:
       - CameraSystem.java
       - ClimberSystem.java
       - DriveSystem.java
       - IntakeOutakeSystem.java
       - ShooterSystem.java
       - TelemetrySystem.java
    
    * Attempted a command coding framework
    
    * Attempted a start at the Drive System by adding the drive function

---
+++ src/main/java/frc/robot/Systems/DriveSystem.java
@@ -0,0 +1,15 @@
+package frc.robot.Systems;
+
+import org.ejml.equation.Variable;
+
+import com.revrobotics.spark.SparkMax;
+
+public class DriveSystem {
+
+    public drive(SparkMax RightMotorA, SparkMax RightMotorB, SparkMax LeftMotorA, SparkMax LeftMotorB) {
+        
+
+
+    }
+    
+}
---
+++ src/main/java/frc/robot/Systems/ShooterSystem.java
@@ -0,0 +1,5 @@
+package frc.robot.Systems;
+
+public class ShooterSystem {
+    
+}
